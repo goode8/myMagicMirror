5d87489934f171588d77a4f9549c4ba3a4ac4d66
goode8 <64971371+goode8@users.noreply.github.com>
2022-10-22 21:14:55 -0700

    added a similar second file to this file

--- calendar.js
+++ calendar.js
@@ -1,3 +1,13 @@
 //path: /MagicMirror/modules/default/calendar
+//filename: calendar.js
+
+//changed:
+fade: false,
+
+//////////////////////////////////////////////////
+Another change:
+//path: /MagicMirror/modules/default/weather/
+//filename: weather.js
+
 //changed:
 fade: false,
