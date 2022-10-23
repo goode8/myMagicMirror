b3e250d9ebe700bd45b1b2d0907c5f351edbc83d
goode8 <64971371+goode8@users.noreply.github.com>
2022-10-22 20:51:42 -0700

    Need to test

---
+++ compliments.js
@@ -0,0 +1,13 @@
+//path: /MagicMirror/modules/default/compliments
+//change:
+defaults: {
+		compliments: {
+			"....-01-01": ["Happy new year!"],
+			"....-04-01": ["April Fools"],
+			"....-12-25":["Merry Christmas"],
+			"....-10-31":["Happy Halloween"],
+			anytime: ["Hello, You"],
+			morning: ["Hello, You", "Enjoy your day!", "How was your sleep?", "Morning, Sunshine"],
+			afternoon: ["Hello, You", "Morning Sunshine", "Looking smart today!"],
+			evening: ["Hello, You", "What's Great About Today?", "Goodnight"],
+		},
