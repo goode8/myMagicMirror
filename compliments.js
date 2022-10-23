7e9f11fbfa9c5eb488dbc718ff671c67dc1c990a
goode8 <64971371+goode8@users.noreply.github.com>
2022-10-22 21:11:03 -0700

    combining two compliment files

--- compliments.js
+++ compliments.js
@@ -7,7 +7,7 @@ defaults: {
 			"....-04-01": ["April Fools"],
 			"....-12-25":["Merry Christmas"],
 			"....-10-31":["Happy Halloween"],
-			anytime: ["Hello, You"],
+			anytime: ["Hello, You", "Hey there, smartypants!"],
 			morning: ["Hello, You", "Enjoy your day!", "How was your sleep?", "Morning, Sunshine"],
 			afternoon: ["Hello, You", "Morning Sunshine", "Looking smart today!"],
 			evening: ["Hello, You", "What's Great About Today?", "Goodnight"],
